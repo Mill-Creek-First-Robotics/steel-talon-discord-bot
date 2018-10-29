ed1f4983a19f5953d54f06dde2ce9e5ab9dd3651
Chris <cheesecakecgcontact@gmail.com>
2018-10-29 14:34:45 -0400

    Added a ping pong subsystem

---
+++ src/subsystem/PingPong.java
@@ -0,0 +1,21 @@
+package subsystem;
+
+import net.dv8tion.jda.*;
+
+public class PingPong {
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event)
+    {
+        if (event.getAuthor().isBot()) return;
+        // We don't want to respond to other bot accounts, including ourself
+        Message message = event.getMessage();
+        String content = message.getContentRaw(); 
+        // getContentRaw() is an atomic getter
+        // getContentDisplay() is a lazy getter which modifies the content for e.g. console view (strip discord formatting)
+        if (content.equals("!ping"))
+        {
+            MessageChannel channel = event.getChannel();
+            channel.sendMessage("Pong!").queue(); // Important to call .queue() on the RestAction returned by sendMessage(...)
+        }
+    }
+}
