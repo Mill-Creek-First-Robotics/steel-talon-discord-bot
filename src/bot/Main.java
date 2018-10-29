945aaf24eb5cc4f9a601f5fd5b90cd96470cf216
Chris <cheesecakecgcontact@gmail.com>
2018-10-29 14:15:40 -0400

    project init

---
+++ src/bot/Main.java
@@ -0,0 +1,14 @@
+package bot;
+
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.JDABuilder;
+
+public class Main {
+
+	public static void main(String[] args) {
+		JDA jda = new JDABuilder("token").build();
+		
+	}
+
+}
+ 
